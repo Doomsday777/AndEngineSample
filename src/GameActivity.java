5eac3b40ae9614216d006a52df00c66b65663bda
Doomsday <Doomsday@Doomsday-PC>
2013-08-23 01:12:29 +0300

    2

--- src/GameActivity.java
+++ src/GameActivity.java
@@ -6,30 +6,23 @@ public class GameActivity extends BaseGameActivity {
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void onCreateResources(
-			OnCreateResourcesCallback pOnCreateResourcesCallback)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onCreateResources( OnCreateResourcesCallback pOnCreateResourcesCallback ) throws Exception {
+		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 
 	@Override
-	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onCreateScene( OnCreateSceneCallback pOnCreateSceneCallback ) throws Exception {
+		Scene oScene = new Scene();
+		pOnCreateSceneCallback.onCreateSceneFinished( oScene );
 	}
 
 	@Override
-	public void onPopulateScene(Scene pScene,
-			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onPopulateScene( Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback ) throws Exception {
+		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
 	
 }
